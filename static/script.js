45e323548171b2c332afd6e8727efc580f94f8ec
fashton28 <fashton502@gmail.com>
2024-07-24 19:23:34 -0400

    add essay section

---
+++ static/script.js
@@ -0,0 +1,8 @@
+
+let title = document.getElementsByClassName("essay-title");
+
+
+title.addEventListener("click", function(){
+    const arrow = document.getElementById("arrow")
+    arrow.style.transform = 'rotate(90deg)';
+})
