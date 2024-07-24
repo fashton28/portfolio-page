5dff4ed6d56588807178911f916315aa8692c3da
fashton28 <fashton502@gmail.com>
2024-07-23 22:40:33 -0400

    add server and routes

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    return 'Hello from Flask!'
+    return render_template('index.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/writing')
+def writing():
+    return render_template('writing.html')
+
+@app.route('/books')
+def books():
+    return render_template('books.html')
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
